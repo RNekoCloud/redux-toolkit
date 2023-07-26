8808efbb6d29c98c48d25ba94ed0dce3c0ae43ea
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-26 14:35:23 +0700

    feat: add comment

--- src/Store/counterSlice.ts
+++ src/Store/counterSlice.ts
@@ -1,27 +1,38 @@
 // mengimport fungsi createSlice dari Redux Toolkit
 import { createSlice } from "@reduxjs/toolkit";
 
+//mendefinisikan tipe state untuk slice ini
 interface CounterState {
-   count: number;
+   count: number; //properti count dengan tipe number
 }
 
+//mendefinisikan nilai awal(initial state) untuk slice ini
 const initialState: CounterState = {
-   count: 0,
+   count: 0, //nilai awal untuk properti count adalah 0
 };
 
+//membuat slice dengan menggunakan fungsi createSlice
 const counterSlice = createSlice({
-   name: "counter",
-   initialState,
+   name: "counter", //nama untuk Slice ini, digunakan dalam menyimpan data pada store
+   initialState, //menggunakan nilai initialState yang telah didefinisikan sebelumnya
    reducers: {
+      //mendefinisikan aksi (action) untuk slice ini
+
+      //aksi untuk melakukan penambahan 1 pada nilai count
       increment(state) {
-         state.count += 1;
+         state.count += 1; //menambahkan 1 nilai ke property count
       },
+
+      //aksi untuk melakukan pengurangan 1 dari nilai count
       decrement(state) {
-         state.count -= 1;
+         state.count -= 1; //mengurangi 1 nilai ke property count
       },
       //...tambahkan aksi lain jika ada
    },
 });
 
+//mengekspor aksi-aksi yang telah didefinisikan
 export const { increment, decrement } = counterSlice.actions;
+
+//mengekspor reducer dari slice ini, reducer akan mengelola perubahan
 export default counterSlice.reducer;
--- src/Store/index.ts
+++ src/Store/index.ts
@@ -1,14 +1,24 @@
+//mengimpor fungsi configureStore dari Redux Toolkit
 import { configureStore } from "@reduxjs/toolkit";
+
+//mengimpor reducer dari slider 'counter' yang telah kita buat sebelumnya
 import counterReducer from "./counterSlice";
 
+//konfigurasi store menggunakan configureStore
 const store = configureStore({
    reducer: {
-      counter: counterReducer,
+      //menyediakan root reducer untuk store
+
+      counter: counterReducer, //menggunakan reducer 'counter' untuk mengelola state 'counter'
       //...tambahkan reducer lain jika membutuhkan aksi
    },
 });
 
+//menggunakan TypeScript untuk menetapkan tipe RootState sebagai tipe dari state pada store
 export type RootState = ReturnType<typeof store.getState>;
+
+//menggunakan TypeScript untuk menentukan tipe AppDispatch sebagai tipe dari dispatch pada store
 export type AppDispatch = typeof store.dispatch;
 
+//mengekspor store yang telah dikonfigurasi dan siap digunakan dalam aplikasi
 export default store;
