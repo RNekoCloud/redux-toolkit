7bee9e1064a11b20eab1df43ccf639eb0fa8667e
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-07-26 13:59:28 +0700

    add counter

---
+++ src/Components/Counter.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement } from "../Store/counterSlice";
+
+export const Counter = () => {
+   const count = useSelector((state: any) => state.counter.count);
+   const dispatch = useDispatch();
+
+   const handleIncrement = () => {
+      dispatch(increment());
+   };
+
+   const handleDecrement = () => {
+      dispatch(decrement());
+   };
+
+   return (
+      <div>
+         <p>Count: {count}</p>
+         <button onClick={handleIncrement}>Increment</button>
+         <button onClick={handleDecrement}>Decrement</button>
+      </div>
+   );
+};
